e59d0490747a54c1cfd3a0f0df023404facd5535
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-02-06 11:37:06 -0600

    Add functionality to userController.js - function signUpUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,59 @@ const {
 exports.signUpUser = asyncHandler(async (req, res, next) => {
    const {fullname, email, password} = req.body;
 
+   if (!fullname) {
+      return next(messageHandler(res, false, 'Fullname is required', 400));
+   }
+   if (validateFullname(fullname).isValid === false) {
+      const {error} = validateFullname(fullname);
+      return next(messageHandler(res, false, error, 406));
+   }
+
+   if (!email) {
+      return next(messageHandler(res, false, 'Email is required', 400));
+   }
+   if (validateEmail(email).isValid === false) {
+      const {error} = validateEmail(email);
+      return next(messageHandler(res, false, error, 406));
+   }
+
+   if (!password) {
+      return next(messageHandler(res, false, 'Password is required', 400));
+   }
+   if (validatePassword(password).isValid === false) {
+      const {error} = validatePassword(password);
+      return next(messageHandler(res, false, error, 406));
+   }
+
+   /************************* find out if a user already exists *************************/
+   const userAlreadyExists = await User.findOne({email});
+
+   if (userAlreadyExists) {
+      return next(messageHandler(res, false, 'User already exists!', 409));
+
+   }
+   /************************* hashing password *************************/
+   const salt = await bcrypt.genSalt(10);
+   const hashedPassword = await bcrypt.hash(password, salt);
+
+   /************************* create and save a user *************************/
+   const newUser = await new User({
+      fullname,
+      email,
+      password: hashedPassword
+   });
+
+   const user = await newUser.save();
+   /************************* successful response *************************/
+   res.status(201).json({
+      success: true,
+      message: 'User created successfully!',
+      user: {
+         ...user._doc,
+         password: null
+      }
+   });
+
 });
 
 exports.signInUser = asyncHandler(async (req, res, next) => {
