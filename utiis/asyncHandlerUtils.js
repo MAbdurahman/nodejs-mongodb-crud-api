a015ead69d53b7fc63197dd4f8717bdaa1215340
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-02-06 08:29:25 -0600

    Add utils/messageHandlerUtil.js

--- utiis/asyncHandlerUtils.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler=(func)=>(req,res,next)=>{
-   Promise.resolve(func(req,res,next)).catch(next);
-
-}
-
-module.exports = asyncHandler;
---
+++ utiis/messageHandlerUtil.js
@@ -0,0 +1,8 @@
+const messageHandler = (res, success, error, statusCode = 400) =>
+   res.status(statusCode).json({
+      success: false,
+      message: error,
+      statusCode: statusCode
+   });
+
+module.exports = messageHandler;
