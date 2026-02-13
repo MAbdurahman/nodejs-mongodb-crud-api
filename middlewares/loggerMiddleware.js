ea43a7175521c1cc82c1bf9ffd59ec1d53361a0d
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-02-13 04:21:23 -0600

    Update middlewares/loggerMiddleware.js -> add statusCode

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -6,7 +6,7 @@ const logger = function(req, res, next) {
    const currentDate = new Date();
    const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`.bold.italic.yellow;
 
-   console.log(`method: ${req.method}  url: ${req.url}  ${res.statusCode} timestamp: ${formattedDate}`.bold.italic.yellow);
+   console.log(`method: ${req.method}  url: ${req.url} statusCode: ${res.statusCode} timestamp: ${formattedDate}`.bold.italic.yellow);
    next();
 }
 
