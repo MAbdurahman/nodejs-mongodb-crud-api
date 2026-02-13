2381de6d601b46bf45fb3b5c15daa3d622d74566
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-02-13 07:26:28 -0600

    Correct data/usersData.js for es6 modules

--- data/usersData.js
+++ data/usersData.js
@@ -1,96 +1,95 @@
-
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 const usersData = [
    {
       fullname: 'John Doe',
       email: 'johndoe@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'admin',
    },
    {
       fullname: 'William Clark',
       email: 'williamclark@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'James Doe',
       email: 'jamesdoe@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Benjamin Taylor',
       email: 'benjamintaylor@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Lucas Moore',
       email: 'lucasmoore@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Henry Martin',
       email: 'henrymartin@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Alex Taylor',
       email: 'alextaylor@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Mary Jane',
       email: 'maryjane@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Sophia Davis',
       email: 'sophiadavis@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Jane Doe',
       email: 'janedoe@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Isabella Brown',
       email: 'isabellabrown@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Ava Wilson',
       email: 'avawilson@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    },
    {
       fullname: 'Amelia Garcia',
       email: 'ameliagarcia@gmail.com',
-      password: bcryptjs.hashSync('Aa!2qwer', 10),
-      passcode: bcryptjs.hashSync('acc356', 10),
+      password: await bcrypt.hash('Aa!2qwer', 10),
+      passcode: await bcrypt.hash('acc356', 10),
       role: 'user'
    }
 ];
